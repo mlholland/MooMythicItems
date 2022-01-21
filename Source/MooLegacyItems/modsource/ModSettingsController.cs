70a6fc0697c2e72a66a37051e6209377c14429eb
mlholland <mlholland@users.noreply.github.com>
2022-01-20 20:46:29 -0500

    Move git to correct directory
    dangit git UI. be more comprehensible please

---
+++ Source/MooLegacyItems/modsource/ModSettingsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Globalization;
+using System.Text;
+using UnityEngine;
+using Verse;
+using RimWorld;
+
+namespace ModSource
+{
+    public class TestMod_Mod : Mod
+    {
+
+        public static EmptySettings settings;
+
+        public TestMod_Mod(ModContentPack content) : base(content)
+        {
+            settings = GetSettings<EmptySettings>();
+        }
+
+        public override string SettingsCategory() => "TestSettingsTitle".Translate();
+
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            base.DoSettingsWindowContents(inRect);
+
+            settings.DoWindowContents(inRect);
+            
+        }
+    }
+}
