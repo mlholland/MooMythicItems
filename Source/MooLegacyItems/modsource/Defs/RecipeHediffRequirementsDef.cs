1a4ac1626a1086f9382087a99d2aec8ea2a7b13a
mlholland <mlholland@users.noreply.github.com>
2022-04-25 02:19:47 -0400

    Implement Epicurean Delight and Required Scaffolding
    Add new Def which allows existing recipes to have hediff requirements on worker pawns to perform them
    
    Use said new code to create a new meal recipe which requires a mythic effect-gated buff to perform.
    
    Remaining todos:
    - make the resulting meal give a hediff in addition to a thought
     - Figure out how to hide the recipe unless someone gets the relevant mythic item. Maybe gate it beyond a 1-point research option?

---
+++ Source/MooLegacyItems/modsource/Defs/RecipeHediffRequirementsDef.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq; 
+using RimWorld;
+using Verse;
+
+/* These defs all add their values to a large static map, which is then consumed by a harmony patch in bill acceptance to
+ * ensure that only pawns with the required hediffs can accept a job. In order for the job to be accepted, the pawn in question
+ * must possess ANY of the listed hediffs. ATM there's no way to modify this logic - it's just an OR-ing of all the possible hediffDef
+ * options.
+ * 
+ * TODO decide on how to enable debug mode on this - perhaps a settings flag, or just make it part of god mode?
+ */
+namespace MooMythicItems
+{
+    public class RecipeHediffRequirementsDef : Def
+    {
+        public List<HediffDef> hediffDefOptions;
+        public RecipeDef recipeDef;
+
+
+        private static Dictionary<RecipeDef, HashSet<HediffDef>> totalMapping = new Dictionary<RecipeDef, HashSet<HediffDef>>();
+
+        public override IEnumerable<string> ConfigErrors()
+        {
+            foreach (var error in base.ConfigErrors()) yield return error;
+            if (recipeDef == null) yield return "recipeDef cannot be null";
+            if (hediffDefOptions == null || hediffDefOptions.Count == 0) yield return "RequiredHediffDefs cannot be null or empty";
+        }
+
+        // Since the startup code is dependent on referenced Defs, we need to run said code after 
+        // ResolveReferences, rather than during a PostLoad override.
+        public override void ResolveReferences()
+        {
+            base.ResolveReferences();
+            if(!totalMapping.ContainsKey(recipeDef))
+            {
+                totalMapping[recipeDef] = new HashSet<HediffDef>();
+            }
+            HashSet<HediffDef> realList = totalMapping[recipeDef];
+            foreach(HediffDef hediffDef in hediffDefOptions)
+            {
+                realList.Add(hediffDef);
+            }
+        }
+
+        public static bool PawnMeetsHediffRequirement(Pawn p, RecipeDef recipeDef)
+        {
+            if(totalMapping.ContainsKey(recipeDef))
+            {
+                foreach(HediffDef hediffDef in totalMapping[recipeDef])
+                {
+                    if(p.health.hediffSet.HasHediff(hediffDef))
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ Source/MooLegacyItems/modsource/Harmony/Pawn_PawnAllowedToStartAnew.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using HarmonyLib;
+using RimWorld;
+using Verse;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+using Verse.AI;
+
+/* This patch modifies job acceptance to allow some jobs to require the worker to have a specific hediff
+ */
+namespace MooMythicItems
+{
+    [HarmonyPatch(typeof(Bill))]
+    [HarmonyPatch(nameof(Bill.PawnAllowedToStartAnew))]
+    public class Bill_PawnAllowedToStartAnew_Postfix
+    {
+        static void Postfix(Pawn p, Bill __instance, ref bool __result) //pass the __result by ref to alter it.
+        {
+            bool newVal = RecipeHediffRequirementsDef.PawnMeetsHediffRequirement(p, __instance.recipe);
+            if (!newVal && __result)
+            {
+                __result = false;
+                JobFailReason.Is(String.Format("MooMF_RecipeRequiresHediffs".Translate(), p.Name.ToStringShort, __instance.recipe.label));
+            }
+        }
+    }
+}
