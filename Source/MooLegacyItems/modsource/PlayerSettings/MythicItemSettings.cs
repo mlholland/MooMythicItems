9ba9e093f1ba791fa6795d741565e6c484c74891
mlholland <mlholland@users.noreply.github.com>
2022-05-26 18:10:12 -0400

    Settings cleanup
    Added a bunch of tooltips, and removed the unused setting for level based caused defs.

--- Source/MooLegacyItems/modsource/PlayerSettings/MythicItemSettings.cs
+++ Source/MooLegacyItems/modsource/PlayerSettings/MythicItemSettings.cs
@@ -15,11 +15,11 @@ namespace MooMythicItems
         public bool flagMythicOwnersCanCreateNewMythicItems = false;
         public bool flagDebug = false;
         public bool flagStartupDebug = false;
-        public int minimumLevelForSkillItems = 18;
+        // public int minimumLevelForSkillItems = 18;
         public int mythicItemSaveLimit = 100;
         public int individualItemOccurenceLimit = 3;
 
-        private string minimumLevelForSkillItemsString = "18";
+        // private string minimumLevelForSkillItemsString = "18";
         private string mythicItemSaveLimitInputString = "100";
         private string individualItemOccurenceLimitString = "3";
 
@@ -35,7 +35,7 @@ namespace MooMythicItems
             Scribe_Values.Look(ref flagMythicOwnersCanCreateNewMythicItems, "flagMythicOwnersCanCreateNewMythicItems", false, true);
             Scribe_Values.Look(ref flagDebug, "flagDebug", false, true);
             Scribe_Values.Look(ref flagStartupDebug, "flagStartupDebug", false, true);
-            Scribe_Values.Look(ref minimumLevelForSkillItems, "minimumLevelForSkillItems", 18, true);
+            // Scribe_Values.Look(ref minimumLevelForSkillItems, "minimumLevelForSkillItems", 18, true);
             Scribe_Values.Look(ref mythicItemSaveLimit, "mythicItemSaveLimit", 100, true);
             Scribe_Values.Look(ref mythicItemSaveLimitInputString, "mythicItemSaveLimit", "100", true);
             Scribe_Values.Look(ref individualItemOccurenceLimit, "individualItemOccurenceLimit", 3, true);
@@ -47,30 +47,19 @@ namespace MooMythicItems
             Listing_Standard ls = new Listing_Standard();
             ls.Begin(inRect);
             ls.Label("MooMF_SettingsLabel".Translate());
-            /*if (Widgets.ButtonText(new Rect(0f, ls.CurHeight, 180f, 29f), "MooMF_ManualSaveButton".Translate(), true, true, true))
-            {
-                MythicItemSaveUtility.SaveMythicItemsFile(testList);
-            } 
-            ls.Gap(30f);
-
-            if (Widgets.ButtonText(new Rect(0f, ls.CurHeight, 180f, 29f), "MooMF_ManualLoadBUtton".Translate(), true, true, true))
-            {
-                List<MythicItem> loadedList = MythicItemSaveUtility.LoadMythicItemsFile();
-            } 
-            ls.Gap(30f);
-            */
             ls.CheckboxLabeled("MooMF_CreateRandomItemsIfNeeded".Translate(), ref flagCreateRandomMythicItemsIfNoneAvailable, "MooMF_CreateRandomItemsIfNeededTooltip".Translate());
-            ls.CheckboxLabeled("MooMF_FlagNotifyItemCreation".Translate(), ref flagNotifyItemCreation, null);
+            ls.CheckboxLabeled("MooMF_FlagNotifyItemCreation".Translate(), ref flagNotifyItemCreation, "MooMF_FlagNotifyItemCreationTooltip".Translate());
             ls.CheckboxLabeled("MooMF_FlagMythicOwnersCanCreateNewMythicItems".Translate(), ref flagMythicOwnersCanCreateNewMythicItems, "MooMF_FlagMythicOwnersCanCreateNewMythicItemsToolTip".Translate());
-            ls.CheckboxLabeled("MooMF_PrintDebugLogs".Translate(), ref flagDebug, null);
-            ls.CheckboxLabeled("MooMF_PrintExtraStartupDebugLogs".Translate(), ref flagStartupDebug, null);
-            ls.Label("MooMF_MinLevelForSkillItems".Translate() + ": " + minimumLevelForSkillItems, -1, "MooMF_MinLevelForSkillItemsToolTip".Translate());
-            ls.IntEntry(ref minimumLevelForSkillItems, ref minimumLevelForSkillItemsString);
+            ls.CheckboxLabeled("MooMF_PrintDebugLogs".Translate(), ref flagDebug, "MooMF_PrintDebugLogsTooltip".Translate());
+            ls.CheckboxLabeled("MooMF_PrintExtraStartupDebugLogs".Translate(), ref flagStartupDebug, "MooMF_PrintExtraStartupDebugLogsTooltip".Translate());
+            // not currently used, level thresholds are set in the cause defs.
+            //ls.Label("MooMF_MinLevelForSkillItems".Translate() + ": " + minimumLevelForSkillItems, -1, "MooMF_MinLevelForSkillItemsToolTip".Translate());
+            //ls.IntEntry(ref minimumLevelForSkillItems, ref minimumLevelForSkillItemsString);
             ls.Label("MooMF_MaxMythicItemsSaved".Translate() + ": " + mythicItemSaveLimit, -1);
             ls.IntEntry(ref mythicItemSaveLimit, ref mythicItemSaveLimitInputString);
             ls.Label("MooMF_MaxItemOccurence".Translate() + ": " + individualItemOccurenceLimit, -1, "MooMF_MaxItemOccurenceTooltip".Translate());
             ls.IntEntry(ref individualItemOccurenceLimit, ref individualItemOccurenceLimitString);
-
+            ls.Label("MooMF_DeleteWarning".Translate(), -1, "MooMF_ClearAllSavedItemsDescription".Translate());
             if (Widgets.ButtonText(new Rect(0f, ls.CurHeight, 180f, 29f), "MooMF_ClearAllSavedItemsButton".Translate(), true, true, true))
             {
                 DebugActions.LogIfDebug(clearItemsPrintoutKey.Translate());
